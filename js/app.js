0bc8b63d2b31d57c05ada35487048ac69358cc65
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-15 15:44:34 +0600

    comolete generate pin part

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-console.log('ami vallo asi')
+function getPin() {
+    let generatePin = Math.round(Math.random() * 10000) + '';
+    if (generatePin.length == 4) {
+        return generatePin
+    }
+    else {
+        return getPin()
+    }
+}
+
+function getGeneratePin() {
+    document.getElementById('click-input').value = getPin();
+}
+
+document.getElementById('type-number').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+
+    const getInputFiled = document.getElementById('calculate-input-filed');
+    const getInputValue = getInputFiled.value;
+
+    if (isNaN(number)) {
+        if (number === 'C') {
+            getInputFiled.value = '';
+        }
+    }
+    else {
+        const newInputValue = getInputValue + number;
+        getInputFiled.value = newInputValue
+    }
+
+})
