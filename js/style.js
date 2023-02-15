1905224e55afd71d68172c56d5ebc0ac1f82ccaa
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-15 18:09:32 +0600

    summery the all website

--- js/style.js
+++ js/style.js
@@ -8,30 +8,54 @@ function getPin() {
     }
 }
 
-function getInputValue(id, value) {
-    const inputFiled = document.getElementById(id);
-    const newInputFiledValue = inputFiled.value = value;
-    return newInputFiledValue
-}
-
 document.getElementById('generatePin').addEventListener('click', function () {
-    const pinNumber = getPin();
-    getInputValue('click-input', pinNumber);
+    document.getElementById('click-input').value = getPin()
 });
 
 document.getElementById('type-number').addEventListener('click', function (event) {
     const number = event.target.innerText;
     const calculateInputFiled = document.getElementById('calculate-input-filed');
+    const previousInputValue = calculateInputFiled.value;
 
     if (isNaN(number)) {
 
         if (number == "C") {
             calculateInputFiled.value = '';
         }
+        else if (number == '<') {
+            let inputValue = previousInputValue.split('');
+            inputValue.pop()
+            calculateInputFiled.value = inputValue.join('')
+        }
     }
     else {
-        const previousInputValue = calculateInputFiled.value;
         const newInputValue = previousInputValue + number;
         calculateInputFiled.value = newInputValue
     }
 })
+
+function getInputValue(id) {
+    const inputFiled = document.getElementById(id);
+    const inputFiledValue = inputFiled.value;
+    return inputFiledValue
+}
+
+function matchPin() {
+    const pinInputValue = getInputValue('click-input');
+    const calculateInputValue = getInputValue('calculate-input-filed');
+    if (pinInputValue == '' || calculateInputValue == '') {
+        return alert('Please provide your number')
+    }
+
+
+    const right = document.getElementById('right');
+    const error = document.getElementById('error');
+    if (pinInputValue == calculateInputValue) {
+        right.style.display = 'block';
+        error.style.display = 'none';
+    }
+    else {
+        error.style.display = 'block';
+        right.style.display = 'none';
+    }
+}
